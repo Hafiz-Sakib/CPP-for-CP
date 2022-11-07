ec016468982f19399592e9a14f65cada1322fbfe
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-07 20:29:56 +0600

    Swaooing Using CPP Refrences

--- Basics of C++/Codes/swap__STL.cpp
+++ Basics of C++/Codes/swap__STL.cpp
@@ -6,21 +6,13 @@ using namespace std;
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-void swapping(int &n, int &m)
-{
-    int t;
-    t = n;
-    n = m;
-    m = t;
-}
-
 int main()
 {
     Boost;
     int a = 3;
     int b = 5;
     cout << "Before Swap : " << a << " " << b << endl;
-    swapping(a, b);
+    swap(a, b);
     cout << "After Swap : " << a << " " << b << endl;
     return 0;
 }
