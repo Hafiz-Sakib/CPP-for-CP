b8a3ecd4d36ab2ba70308db6bc4a0bd2f99771b3
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-07 13:27:04 +0600

    All About Fixed and Setprecison

--- Basics of C++/Codes/SetPrecision.cpp
+++ Basics of C++/Codes/SetPrecision.cpp
@@ -7,11 +7,19 @@ using namespace std;
     cout.tie(NULL)
 int main()
 {
+
     Boost;
+
+    //যখন শুধুমাত্র Setprecision(num) দিব ,তখন দশমিকের  আগে ও পরে ,মিলে Setprecision() এর ভিতরের থাকা নাম্বার সংখ্যক (এখানের জন্য ৫ ) ডিজিট প্রিন্ট করবে
+
+    //কিন্ত যখন Setprecision()  এর আগে fixed  ব্যবহার করব তখন শুধুমাত্র দশমিক এর পরে setprecision() এর ভিতরে থাকা number এর  সংখ্যক ডিজিট প্রিন্ট করবে
+
     double a = 10.10;
     double b = 100.1546;
-    cout << a * b << endl;                             // 1011.56
-    cout << fixed << setprecision(1) << a * b << endl; // 1011.56
+    cout << a * b << endl;
+    cout << setprecision(1) << a * b << endl;
+
+    cout << fixed << setprecision(1) << a * b << endl;
 
     return 0;
 }
--- Basics of C++/Codes/fixed.cpp
+++ Basics of C++/Codes/fixed.cpp
@@ -11,6 +11,18 @@ int main()
     double a = 1.22546541;
     double b = 4.2241;
     double c = a + b;
-    cout << fixed << setprecision(20) << c << endl; // setprecision er vitore thaka number er soman doshomik er por print korbe
+
+    cout << setprecision(5) << c << endl;
+
+    //যখন শুধুমাত্র Setprecision(num) দিব ,তখন দশমিকের  আগে ও পরে ,মিলে Setprecision() এর ভিতরের থাকা নাম্বার সংখ্যক (এখানের জন্য ৫ ) ডিজিট প্রিন্ট করবে
+    // output : 13.123
+    //এখামে দশমিকের আগে দুই ডিজিট ১৩ ও দশমিকের পরে তিন ডিজিট ১২৩ প্রিন্ট করবে
+
+    cout << fixed << setprecision(2) << c << endl;
+
+    //কিন্ত যখন Setprecision()  এর আগে fixed  ব্যবহার করব তখন শুধুমাত্র দশমিক এর পরে setprecision() এর ভিতরে থাকা number এর  সংখ্যক ডিজিট প্রিন্ট করবে
+    // output : 13.12
+    //এখামে দশমিকের পরে শুধুমাত্র দুই ডিজিট প্রিন্ট করবে
+
     return 0;
 }
