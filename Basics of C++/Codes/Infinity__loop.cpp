7007a4cdde6bfb89e31846d58eb17121276ddc9a
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-07 11:10:00 +0600

    Infinite Loops

---
+++ Basics of C++/Codes/Infinity__loop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    while (true)
+    {
+        cout << "Hello" << endl;
+    }
+
+    for (;;)
+    {
+        cout << "Hello" << endl;
+    }
+
+    return 0;
+}
