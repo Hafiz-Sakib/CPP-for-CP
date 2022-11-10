7d221129e3e1233aaa833dd88c6793612fe0e753
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-10 10:55:45 +0600

    STL Initialized

--- Basics of C++/Codes/test.cpp
+++ Basics of C++/Codes/test.cpp
@@ -1,24 +1,60 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-#define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
 
-void increament(int &n, int m)
+class batsman
 {
-    n++;
-    m++;
-}
+private:
+    int batsman_code;
+    string batsman_name;
+    double total_innings, notout_innings, total_runs, batting_avg;
+
+    double calcavg()
+    {
+        return total_runs / (total_innings - notout_innings);
+    }
+
+public:
+    void readdata(int code, string name, double total, double notout, int runs)
+    {
+        batsman_name = name;
+        batsman_code = code;
+        total_innings = total;
+        notout_innings = notout;
+        total_runs = runs;
+        batting_avg = calcavg();
+    }
+
+    void displaydata()
+    {
+        cout << "The Data members are:" << endl;
+        cout << "Batsman code:" << batsman_code << endl;
+        cout << "Batsman name:" << batsman_name << endl;
+        cout << "Total Innings:" << total_innings << endl;
+        cout << "Notout Innings:" << notout_innings << endl;
+        cout << "Total Runs : " << total_runs << endl;
+        cout << "Batting Average :" << batting_avg << endl;
+    }
+};
 
 int main()
 {
-    Boost;
-    int a = 3;
-    int b = 5;
-    cout << a << " " << b << endl;
-    increament(a, b);
-    cout << a << " " << b << endl;
+    string name;
+    int code;
+    double total, notout, runs;
+    cout << "Enter Batsman code:";
+    cin >> code;
+    cout << "Enter Batsman name:";
+    cin >> name;
+    cout << "Enter Total Innings:";
+    cin >> total;
+    cout << "Enter Notout Innings:";
+    cin >> notout;
+    cout << "Enter Total Runs : ";
+    cin >> runs;
+
+    batsman batsman1;
+    batsman1.readdata(code, name, total, notout, runs);
+    batsman1.displaydata();
+
     return 0;
 }
