aa8a4fe32061706a62997ad8228d0b171aa041a4
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-10 20:44:07 +0600

    pair input

---
+++ STL/Codes/3_Pair__Input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    pair<int, int> p;
+
+    cin >> p.first >> p.second;
+
+    cout << p.first << " " << p.second;
+
+    return 0;
+}
