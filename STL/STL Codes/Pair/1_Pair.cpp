cad55c47e6c22ee115780522eb5d303e43e7b8ee
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-11 10:05:59 +0600

    pairs

--- STL/STL Codes/Pair/1_Pair.cpp
+++ STL/STL Codes/Pair/1_Pair.cpp
@@ -1,49 +1,49 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-#define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+#define Boost                          \
+     ios_base::sync_with_stdio(false); \
+     cin.tie(NULL);                    \
+     cout.tie(NULL)
 int main()
 {
-    Boost;
+     Boost;
 
-    //  declaration :
-    pair<int, string> p;
+     //  declaration :
+     pair<int, string> p;
 
-    // initialization:
+     // initialization:
 
-    p = make_pair(5, "Sakib");
-    // or
+     p = make_pair(5, "Sakib");
+     // or
 
-    p = {10, "Hafiz"};
+     p = {10, "Hafiz"};
 
-    // print :
-    cout << p.first << " " << p.second;
+     // print :
+     cout << p.first << " " << p.second;
 
-    // make copy by value :
-    pair<int, string> p1;
+     // make copy by value :
+     pair<int, string> p1;
 
-    p1 = p;
+     p1 = p;
 
-    p1 = {15, "Harry"};
+     p1 = {15, "Harry"};
 
-    cout << endl
-         << p1.first << " " << p1.second;
+     cout << endl
+          << p1.first << " " << p1.second;
 
-    // make copy by refrences
+     // make copy by refrences
 
-    pair<int, string> &p2 = p; // p2 er kisu change kole p teo change hye jaabe
+     pair<int, string> &p2 = p; // p2 er kisu change kole p teo change hye jaabe
 
-    p2.first = 16;
-    p2.second = "ChangedSakib";
+     p2.first = 16;
+     p2.second = "ChangedSakib";
 
-    cout << endl
-         << p.first << " " << p.second;
+     cout << endl
+          << p.first << " " << p.second;
 
-    cout << endl
-         << p2.first << " " << p2.second;
+     cout << endl
+          << p2.first << " " << p2.second;
 
-    return 0;
+     return 0;
 }
