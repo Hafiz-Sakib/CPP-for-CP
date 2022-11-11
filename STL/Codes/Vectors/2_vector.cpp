414d861da43f25138b30f992e6603378358b8a21
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-11 09:53:26 +0600

    vector

---
+++ STL/Codes/Vectors/2_vector.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void printVec(vector<int> v)
+{
+    cout << "size : " << v.size() << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Boost;
+    vector<int> v;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+
+    return 0;
+}
