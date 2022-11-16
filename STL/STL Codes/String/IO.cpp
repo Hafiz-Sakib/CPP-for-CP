65a7ad9150e1228fcab4887b7cd6ae3d701e9902
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-16 10:28:35 +0600

    string

---
+++ STL/STL Codes/String/IO.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    string s, p, d;
+    cin >> s;
+    cout << s << endl;
+    cin >> p;
+    cout << p << endl;
+    cin >> d;
+    cout << d << endl;
+    return 0;
+}
