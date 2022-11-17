e8ada5315d4de7ac9b2844412dac792c1de2c411
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-17 21:11:24 +0600

    sort in array of pairs

---
+++ STL/STL Codes/Pair/6_array__of__pair.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    pair<int, int> p[] = {{6, 5}, {2, 3}};
+    sort(p, p + 2);
+    for (int i = 0; i < sizeof(p); i++)
+    {
+        cout << p[i].first << " " << p[i].second << endl;
+    }
+}
