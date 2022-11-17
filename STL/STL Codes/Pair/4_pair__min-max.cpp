5fcb7d10508c96caf9d5bc162e702aec8415672c
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-17 20:24:59 +0600

    pair min and max

--- STL/STL Codes/Pair/4_pair__min-max.cpp
+++ STL/STL Codes/Pair/4_pair__min-max.cpp
@@ -12,7 +12,7 @@ int main()
     p1 = {5, 6};
     p2 = {2, 3};
 
-    pair<int, int> p = max(p1, p2);
+    pair<int, int> p = max(p1, p2); // it will check regarding 1st element of the pairs
 
     cout << p.first << " " << p.second << endl;
 
