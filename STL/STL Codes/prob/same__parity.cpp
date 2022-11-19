97bc41b350f7b9e29ff99a734c60e88786ba4cd7
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-19 10:48:08 +0600

    prob

---
+++ STL/STL Codes/prob/same__parity.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+
+    return 0;
+}
