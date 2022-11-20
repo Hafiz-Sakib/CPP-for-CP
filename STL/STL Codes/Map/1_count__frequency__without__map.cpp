3b6578e93e61dbd72a52cf64d1069b2915a0129c
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-20 20:04:23 +0600

    count frequency without map

---
+++ STL/STL Codes/Map/1_count__frequency__without__map.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+int cnt[100231];
+
+int main()
+{
+    Boost;
+
+    vector<int> v = {2, 3, 3, 3, 4, 3, 3, 2, 2, 1, 8};
+    for (auto u : v)
+    {
+        cnt[u]++;
+    }
+    for (int i = 0; i < 100231; i++)
+    {
+        if (cnt[i] > 0)
+        {
+            cout << i << " is occured " << cnt[i] << " "
+                 << "times" << endl;
+        }
+    }
+
+    return 0;
+}
