f33c8aa7de7921dec74942c9bfd1aaf45c622035
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-21 16:50:13 +0600

    Map USer Input

---
+++ STL/STL Codes/Map/6_map__user__input.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    map<string, int> m;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        int a;
+        cin >> s >> a;
+        m[s] = a;
+    }
+    cout << "Your Inputed Data : " << endl;
+    for (auto u : m)
+    {
+        cout << u.first << " " << u.second << endl;
+    }
+
+    return 0;
+}
