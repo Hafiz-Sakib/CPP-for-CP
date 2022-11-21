d43a2c70107d4e18a75b1e8db29c66af0f5cc6e2
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-21 10:16:38 +0600

    count frequency with map

--- STL/STL Codes/Map/3_count__frequency__with__map.cpp
+++ STL/STL Codes/Map/3_count__frequency__with__map.cpp
@@ -8,22 +8,25 @@ using namespace std;
 int main()
 {
     Boost;
-    vector<long long int> v = {1564874541897, 1564874541897, 1564874541897, 658498798, 89787};
+    vector<long long int> v = {1564874541897, 1564874541897, 1564874541897, 658498798, 89787, -1, -2, -59, -59, -59, -59, -59, -59, -59, -59, -59, -59};
     map<long long int, long long int> m;
 
-    for (int i = 0; i < v.size(); i++)
-    {
-        m[v[i]]++;
-    }
+    /*
+     for (int i = 0; i < v.size(); i++)
+        {
+            m[v[i]]++;
+        }
 
-    cout << m[1564874541897] << endl;
+        cout << m[1564874541897] << endl;
+
+    */
 
     for (auto u : v)
     {
         m[u]++;
     }
 
-    cout << m[1564874541897] << endl;
+    cout << m[-59] << endl;
 
     return 0;
 }
