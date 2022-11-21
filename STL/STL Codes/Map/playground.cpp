55c5581c286bd1d35c6f0c427dacdf0fb68e796d
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-21 13:18:53 +0600

    playground

---
+++ STL/STL Codes/Map/playground.cpp
@@ -0,0 +1,74 @@
+// Bismillahir Rahmanir Rahim
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pob pop_back
+#define in insert
+#define vi vector<ll int>
+#define vs vector<string>
+#define pii pair<ll int, ll int>
+#define psi pair<string, ll int>
+#define pis pair<ll int, string>
+#define mii map<ll int, ll int>
+#define mib map<ll int, bool>
+#define msi map<string, ll int>
+#define mis map<ll int, string>
+#define S string
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define b() begin()
+#define e() end()
+#define cY cout << "YES\n"
+#define cN cout << "NO\n"
+#define cy cout << "Yes\n"
+#define cn cout << "No\n"
+#define read3         \
+    long int a, b, c; \
+    cin >> a >> b >> c;
+#define read2      \
+    long int a, b; \
+    cin >> a >> b;
+#define read1   \
+    long int a; \
+    cin >> a;
+#define write1 cout << a << newline;
+#define write2 cout << a << newline << b << newline;
+#define write3 cout << a << newline << b << newline << c << newline;
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define setp(n) fixed << setprecision(n)
+#define mem(arr, val) memset(arr, val, sizeof(arr));
+#define debug(x) cerr << x << newline;
+#define here fprintf(stderr, "====I am Here====\n");
+#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define maxx 100005
+#define EPS 1e-9
+using namespace std;
+const double PI = acos(-1);
+ll int hp = 1e9 + 7;
+
+void Boom()
+{
+    // Let's Move
+}
+
+int main()
+{
+    Boost;
+
+    string s = "sakib";
+    sort(rall(s));
+    cout << s;
+
+    return 0;
+}
