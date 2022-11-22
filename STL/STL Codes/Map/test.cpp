a23148bf2f3f7aebc33dbe3c890bd71aa7538b5e
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-22 09:43:03 +0600

    tets

--- STL/STL Codes/Map/test.cpp
+++ STL/STL Codes/Map/test.cpp
@@ -1,74 +1,30 @@
-// Bismillahir Rahmanir Rahim
+#include <iostream>
 #include <bits/stdc++.h>
-#define ull unsigned long long
-#define ll long long
-#define pb push_back
-#define mp make_pair
-#define pob pop_back
-#define in insert
-#define vi vector<ll int>
-#define vs vector<string>
-#define pii pair<ll int, ll int>
-#define psi pair<string, ll int>
-#define pis pair<ll int, string>
-#define mii map<ll int, ll int>
-#define mib map<ll int, bool>
-#define msi map<string, ll int>
-#define mis map<ll int, string>
-#define S string
-#define fi first
-#define se second
-#define space ' '
-#define endl "\n"
-#define all(data) data.begin(), data.end()
-#define rall(data) data.rbegin(), data.rend()
-#define b() begin()
-#define e() end()
-#define cY cout << "YES\n"
-#define cN cout << "NO\n"
-#define cy cout << "Yes\n"
-#define cn cout << "No\n"
-#define read3         \
-    long int a, b, c; \
-    cin >> a >> b >> c;
-#define read2      \
-    long int a, b; \
-    cin >> a >> b;
-#define read1   \
-    long int a; \
-    cin >> a;
-#define write1 cout << a << newline;
-#define write2 cout << a << newline << b << newline;
-#define write3 cout << a << newline << b << newline << c << newline;
-#define fr(a, b) for (int i = a; i < b; i++)
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define setp(n) fixed << setprecision(n)
-#define mem(arr, val) memset(arr, val, sizeof(arr));
-#define debug(x) cerr << x << newline;
-#define here fprintf(stderr, "====I am Here====\n");
-#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
+using namespace std;
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
-#define maxx 100005
-#define EPS 1e-9
-using namespace std;
-const double PI = acos(-1);
-ll int hp = 1e9 + 7;
-
-void Boom()
-{
-    // Let's Move
-}
-
 int main()
 {
     Boost;
+    vector<int> v = {2, 8, 4, 1};
+    auto c = v;
 
-    string s = "sakib";
-    sort(rall(s));
-    cout << s;
+    // sort(v.begin(), v.end());
 
+    //  sort(v.rbegin(), v.rend());
+
+    for (auto u : v)
+    {
+        cout << u << " ";
+    }
+    cout << endl;
+
+    for (auto u : c)
+    {
+        cout << u << " ";
+    }
+    cout << endl;
     return 0;
 }
