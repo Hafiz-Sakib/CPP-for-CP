38f85e1145b7a541b13137cef541105b1b0d7dbe
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-24 00:34:35 +0600

    cf1

--- STL/STL Codes/Map/cf1.cpp
+++ STL/STL Codes/Map/cf1.cpp
@@ -16,6 +16,22 @@ using namespace std;
 int main()
 {
     Boost;
+    int n;
+    cin >> n;
+
+    map<string, bool> vis;
+
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        if (vis[s] == 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+
+        vis[s] = 1;
+    }
 
     return 0;
 }
