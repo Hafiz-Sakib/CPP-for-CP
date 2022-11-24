422c996847a7b0582f5106c387a821025d1a30ca
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-24 13:10:25 +0600

    Box

---
+++ STL/STL Codes/Map/cf4.cpp
@@ -0,0 +1,47 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+int a[100000 + 23];
+int main()
+{
+    Boost;
+    int box;
+    cin >> box;
+    map<int, int> count;
+
+    for (int i = 0; i < box; i++)
+    {
+        cin >> a[i];
+        count[a[i]]++;
+    }
+
+    int maxi = 0;
+
+    for (auto u : count)
+    {
+
+        if (u.second > maxi)
+        {
+            maxi = u.second;
+        }
+
+        // maxi = max(maxi, u.second);
+    }
+
+    cout << maxi;
+
+    return 0;
+}
