4f1e8d639a4b6974095ed53429522721d3d6bb3b
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-25 21:36:55 +0600

    string upper

---
+++ STL/STL Codes/Set/problem solve/strings__upper.cpp
@@ -0,0 +1,27 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+#define all(data) data.begin(), data.end()
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    string s = "heLlo tHiS iS sAkIb bRo";
+    stringUpper(s);
+    cout << s;
+
+    return 0;
+}
