239a95c980f9dd58ee9a007448cad65127f8073d
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-26 16:15:45 +0600

    leetcode

--- STL/STL Codes/Set/problem solve/leetcode__set.cpp
+++ STL/STL Codes/Set/problem solve/leetcode__set.cpp
@@ -1,3 +1,5 @@
+
+
 /*
 
 string author;
@@ -17,24 +19,22 @@ int main()
 {
     Boost;
     vector<int> nums = {7, 10, 5, 5, 6, 6, 4, 10, 5, 4, 9, 4, 9, 6, 5, 9, 6, 3, 6, 5, 6, 7, 7, 4, 9, 9, 10, 5, 8, 1, 8, 3, 2, 7, 5, 10, 1, 8, 5, 8, 4, 3, 6, 4, 9, 4, 2, 8, 3, 2, 2, 1, 5, 6, 3, 2, 6, 1, 8, 6, 2, 9, 1, 4, 5, 10, 8, 5, 10, 5, 10, 1, 4, 8, 3, 6, 4, 10, 9, 1, 1, 1, 2, 2, 9, 6, 6, 8, 1, 9, 2, 5, 5, 2, 1, 8, 5, 2, 3, 10};
-    map<int, int> m;
+
+    set<int> s;
     for (auto u : nums)
     {
-        m[u]++;
+        s.insert(u);
     }
-    for (auto u : m)
+
+    if (s.size() == nums.size())
     {
-        if (u.second >= 2)
-        {
-            cout << "True" << endl;
-            break;
-        }
-        else
-        {
-            cout << "False" << endl;
-            break;
-        }
+        cout << "False" << endl;
     }
+    else
+    {
+        cout << "True" << endl;
+    }
+
     return 0;
 }
 
