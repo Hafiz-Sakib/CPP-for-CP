8b688491f8821a90c41fccd37870228d3f6d5829
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-29 19:12:43 +0600

    stack print

---
+++ STL/STL Codes/Stack/3__print__stack.cpp
@@ -0,0 +1,38 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(7);
+
+    cout << "Stack Size is : " << s.size() << endl;
+
+    while (!s.empty())
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
+    cout << "Stack Size is : " << s.size() << endl;
+
+    return 0;
+}
