4bb204c3769ffbef087ed350428bb262b4dc60a1
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-29 11:41:07 +0600

    Set

---
+++ STL/STL Codes/Stack/set__upper.cpp
@@ -0,0 +1,22 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    set<int> s = {2, 6, 8, 1, 2, 3, 5, 4, 7, 8, 9, 8};
+
+    return 0;
+}
