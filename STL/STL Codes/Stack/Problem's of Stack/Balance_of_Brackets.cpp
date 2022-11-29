bb27382d46581cf757fd0d47b2d05180578860f9
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-29 23:41:20 +0600

    Check Balanced Brackets Using Stack

--- STL/STL Codes/Stack/Problem's of Stack/Balance_of_Brackets.cpp
+++ STL/STL Codes/Stack/Problem's of Stack/Balance_of_Brackets.cpp
@@ -1,6 +1,18 @@
-/// In the name of ALLAH
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 
 bool isBalanced(char c1, char c2)
 {
@@ -9,6 +21,7 @@ bool isBalanced(char c1, char c2)
 
 int main()
 {
+    Boost;
     int t;
     cin >> t;
 
