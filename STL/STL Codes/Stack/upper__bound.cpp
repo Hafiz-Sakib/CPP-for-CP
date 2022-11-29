26804d581baef80a04a2c8021cccec7ebedfb1ef
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-29 12:43:31 +0600

    lower and upper bond in vector and set

--- STL/STL Codes/Stack/upper__bound.cpp
+++ STL/STL Codes/Stack/upper__bound.cpp
@@ -1,13 +1,21 @@
-#include "bits/stdc++.h"
+/*
 
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 int main()
 {
-    ios::sync_with_stdio(false);
-
+    Boost;
     vector<int> s = {6, 6, 8, 1200, 1400, 1600, 1900, 2100, 2300, 2400, 2600, 3000};
 
     int n;
@@ -21,6 +29,5 @@ int main()
     {
         cout << "Upper Bound of " << n << " is : " << *it << endl;
     }
-
     return 0;
 }
