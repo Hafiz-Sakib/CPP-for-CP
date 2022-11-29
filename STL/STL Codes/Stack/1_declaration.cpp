ba0a4c29df213f56c51b05a558bf652290c4be3b
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-29 19:06:52 +0600

    stack declaretion

---
+++ STL/STL Codes/Stack/1_declaration.cpp
@@ -0,0 +1,30 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(7);
+
+    cout << s.top(); // will show the lastest element added in stack / top element of stack
+
+    return 0;
+}
