cdb93f829cf93054af6076b61d8bb3ce6ea1362e
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-01 11:13:27 +0600

    Check Front Element

---
+++ STL/STL Codes/Queue/check__FrontElement.cpp
@@ -0,0 +1,30 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(15);
+    q.push(11);
+    q.push(12);
+
+    cout << q.front() << endl;
+
+    return 0;
+}
