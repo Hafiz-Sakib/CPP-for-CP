ae0daa0eb5a161aa8ae5e64c162b467ffda1b653
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-01 19:01:23 +0600

    Clear Reverse

---
+++ STL/STL Codes/Queue/Problems Solved/clear__reverse.cpp
@@ -0,0 +1,51 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+int main()
+{
+    Boost;
+    queue<int> q;
+    stack<int> s;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        q.push(a);
+    }
+
+    while (!q.empty())
+    {
+        s.push(q.front());
+        q.pop();
+    }
+
+    while (!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    cout << endl;
+
+    return 0;
+}
