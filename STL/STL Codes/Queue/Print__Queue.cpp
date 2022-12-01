a7fd1d5ead6355b3cf8271654680ab70f218f16d
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-01 11:23:45 +0600

    Print a Queueeeeeeee

---
+++ STL/STL Codes/Queue/Print__Queue.cpp
@@ -0,0 +1,36 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(15);
+    q.push(11);
+    q.push(12);
+
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    cout << endl;
+
+    return 0;
+}
