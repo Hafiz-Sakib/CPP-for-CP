0e04aab2c7895def814cb80fd6df292508a70c10
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-01 11:05:25 +0600

    Stack User Input

---
+++ STL/STL Codes/Queue/stack__Input.cpp
@@ -0,0 +1,31 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    queue<int> q;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        q.push(a);
+    }
+
+    return 0;
+}
