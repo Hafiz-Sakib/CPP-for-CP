29e409e8c3ca66937050464838cf1804d1afe2d4
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-03 00:20:35 +0600

    update

--- STL/STL Codes/Priority Queue/test.cpp
+++ STL/STL Codes/Priority Queue/test.cpp
@@ -22,26 +22,8 @@ int main()
     {
         int n;
         cin >> n;
-        vector<int> vec(n);
-        for (int i = 0; i < n; i++)
-            cin >> vec[i];
-        cout << n - 1 << endl;
-        if (n > 1)
-        {
-            cout << 1 << " " << n << endl;
-            int x;
-            if ((vec[0] + vec[n - 1]) % 2 == 0)
-                vec[0] = vec[n - 1], x = vec[n - 1];
-            else
-                vec[n - 1] = vec[0], x = vec[0];
-            for (int i = 2; i < n - 1; i++)
-            {
-                if ((x + vec[i]) % 2 == 0)
-                    cout << i + 1 << " " << n << endl;
-                else
-                    cout << 1 << " " << i + 1 << endl;
-            }
-        }
+        n -= 3;
+        cout << n / 3 - 1 << "\n";
     }
     return 0;
 }
