15d54b81f65ee4f1043252fa4d6d72b84c0512ee
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-03 00:12:25 +0600

    PQ Input

---
+++ STL/STL Codes/Priority Queue/pq__input.cpp
@@ -0,0 +1,38 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    priority_queue<int> q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        q.push(a);
+    }
+
+    cout << "Output : " << endl;
+
+    while (!q.empty())
+    {
+        cout << q.top() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
