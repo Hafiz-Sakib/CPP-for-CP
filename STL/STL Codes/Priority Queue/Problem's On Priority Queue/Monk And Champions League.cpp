86961b6434e09fcceef5ec0077c37479f84f3154
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-03 23:33:43 +0600

    Monk And Champions League

---
+++ STL/STL Codes/Priority Queue/Problem's On Priority Queue/Monk And Champions League.cpp
@@ -0,0 +1,23 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+
+    return 0;
+}
+
+// https://www.hackerearth.com/practice/data-structures/trees/heapspriority-queues/practice-problems/algorithm/monk-and-champions-league/
