d6bf3f3f5aada14d533bcb694b5bbcb43b417519
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-04 20:35:19 +0600

    update

--- STL/STL Codes/Priority Queue/test.cpp
+++ STL/STL Codes/Priority Queue/test.cpp
@@ -1,81 +1,29 @@
-// Bismillahir Rahmanir Rahim
-/*
-
-string author;
-author = Hafiz_Sakib;
-
-*/
 #include <bits/stdc++.h>
-#define ull unsigned long long
-#define ll long long
-#define pb push_back
-#define mp make_pair
-#define pob pop_back
-#define in insert
-#define vi vector<ll int>
-#define vs vector<string>
-#define pii pair<ll int, ll int>
-#define psi pair<string, ll int>
-#define pis pair<ll int, string>
-#define mii map<ll int, ll int>
-#define mib map<ll int, bool>
-#define msi map<string, ll int>
-#define mis map<ll int, string>
-#define S string
-#define fi first
-#define se second
-#define space ' '
+using namespace std;
+#define ll long long int
 #define endl "\n"
-#define all(data) data.begin(), data.end()
-#define rall(data) data.rbegin(), data.rend()
-#define vecMax(data) *max_element(data.begin(), data.end())
-#define vecMin(data) *min_element(data.begin(), data.end())
-#define vecSum(data) accumulate(data.begin(), data.end(), 0)
-#define vecCount(data, key) count(data.begin(), data.end(), key)
-#define Reverse(data) reverse(data.begin(), data.end())
-#define b() begin()
-#define e() end()
-#define YES cout << "YES\n"
-#define NO cout << "NO\n"
-#define yes cout << "Yes\n"
-#define no cout << "No\n"
-#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
-#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
-#define fr(a, b) for (int i = a; i < b; i++)
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define setp(n) fixed << setprecision(n)
-#define mem(arr, val) memset(arr, val, sizeof(arr));
-#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
-#define debug(x) cerr << x << endl;
-#define here fprintf(stderr, "====I am Here====\n");
-#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
-#define Boost                         \
+#define optimize()                    \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
-    cout.tie(NULL)
-#define maxx 100005
-#define EPS 1e-9
-using namespace std;
-const double PI = acos(-1);
-ll int hp = 1e9 + 7;
-const int mx = 1e8 + 123;
-
-void Boom()
-{
-    // Let's Move
-}
-
+    cout.tie(NULL);
 int main()
 {
-    Boost;
-
-    int t = 1;
-    cin >> t;
-    while (t--)
+    optimize();
+    vector<long long int> v(4);
+    for (int i = 0; i < 4; i++)
     {
-        // cout<<"Case "<<t<<" : ";
-        Boom();
+        cin >> v[i];
     }
+
+    sort(v.begin(), v.end());
+
+    int size = unique(v.begin(), v.end()) - v.begin();
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << v[i] << ' ';
+    }
+    cout << endl;
 
     return 0;
 }
