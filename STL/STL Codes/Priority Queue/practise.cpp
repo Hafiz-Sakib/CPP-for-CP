2459accd476dace5e694502a3dd3a5460441d815
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-05 01:00:25 +0600

    update

--- STL/STL Codes/Priority Queue/practise.cpp
+++ STL/STL Codes/Priority Queue/practise.cpp
@@ -1,95 +1,166 @@
-// Bismillahir Rahmanir Rahim
-/*
+// In the name of ALLAH
 
-string author;
-author = Hafiz_Sakib;
-
-*/
 #include <bits/stdc++.h>
 using namespace std;
-#define ull unsigned long long
-#define ll long long
-#define vi vector<ll int>
-#define vs vector<string>
-#define pii pair<ll int, ll int>
-#define psi pair<string, ll int>
-#define pis pair<ll int, string>
-#define mii map<ll int, ll int>
-#define mib map<ll int, bool>
-#define msi map<string, ll int>
-#define mis map<ll int, string>
-#define si set<ll int>
-#define ss set<string>
-#define sd set<double>
 
-#define space ' '
-#define endl "\n"
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
 #define F first
 #define S second
-#define pb push_back
-#define mp make_pair
-#define pob pop_back
-#define in insert
-
-#define b() begin()
-#define e() end()
-#define all(data) data.begin(), data.end()
-#define rall(data) data.rbegin(), data.rend()
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(), (a).end()
 #define sz(x) (int)x.size()
-#define vecMax(data) *max_element(data.begin(), data.end())
-#define vecMin(data) *min_element(data.begin(), data.end())
-#define vecSum(data) accumulate(data.begin(), data.end(), 0)
-#define vecCount(data, key) count(data.begin(), data.end(), key)
-#define Reverse(data) reverse(data.begin(), data.end())
-
-#define YES cout << "YES\n"
-#define NO cout << "NO\n"
-#define yes cout << "Yes\n"
-#define no cout << "No\n"
-
-#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
-#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
-#define fr(a, b) for (int i = a; i < b; i++)
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
-#define setp(n) fixed << setprecision(n)
-#define mem(arr, val) memset(arr, val, sizeof(arr));
-#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
-
-#define debug(x) cerr << x << endl;
-#define here fprintf(stderr, "====I am Here====\n");
-
-#define gcd(a, b) __gcd(a, b)
-#define sqr(a) ((a) * (a))
-#define MOD 1000000007
+#define mid(l, r) ((r + l) / 2)
+#define left(node) (node * 2)
+#define right(node) (node * 2 + 1)
+#define mx_int_prime 999999937
 
 const double PI = acos(-1);
-const int mx = 1e9 + 7;
 const double eps = 1e-9;
 const int inf = 2000000000;
 const ll infLL = 9000000000000000000;
+#define MOD 1000000007
 
-#define Boost                         \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);                    \
-    cout.tie(NULL)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define gcd(a, b) __gcd(a, b)
+#define sqr(a) ((a) * (a))
 
-void Boom()
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+int dx[] = {0, 0, +1, -1};
+int dy[] = {+1, -1, 0, 0};
+// int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+// int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, const pair<F, S> &p)
 {
-    // Let's Move
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "{";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const multiset<T> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, const map<F, S> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << it->first << " = " << it->second;
+    }
+    return os << "]";
+}
+
+#define dbg(args...)            \
+    do                          \
+    {                           \
+        cerr << #args << " : "; \
+        faltu(args);            \
+    } while (0)
+
+void faltu()
+{
+    cerr << endl;
+}
+
+template <typename T>
+void faltu(T a[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename... hello>
+void faltu(T arg, const hello &...rest)
+{
+    cerr << arg << ' ';
+    faltu(rest...);
 }
 
 int main()
 {
-    Boost;
-
-    int t = 1;
-    cin >> t;
-    while (t--)
+    optimize();
+    vector<pair<int, string>> v(2);
+    for (int i = 0; i < 2; i++)
     {
-        // cout<<"Case "<<t<<" : ";
-        Boom();
+        cin >> v[i].first >> v[i].second;
+        int a;
+        string b;
+        cin >> a >> b;
+        v[i] = make_pair(a, b);
+        v[i] = {a, b};
     }
+    for (int i = 0; i < 2; i++)
+    {
+        cout << v[i].first << " " << v[i].second << endl;
+    }
+
+    dbg(v);
 
     return 0;
 }
