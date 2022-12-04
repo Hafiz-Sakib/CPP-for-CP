f0906ff19f1e4949629e272a416ef9ddf2e696d1
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-04 13:01:39 +0600

    Updateeeeeeeeeee!!!!!!!

---
+++ STL/STL Codes/Priority Queue/vector__sum.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+#define vecCount(data, key) count(data.begin(), data.end(), key)
+#define Reverse(data) reverse(data.begin(), data.end())
+#define optimize()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+int main()
+{
+    optimize();
+
+    vector<long long int> v(4);
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> v[i];
+    }
+
+    int sum = vecSum(v);
+    cout << sum << endl;
+
+    int Max = vecMax(v);
+    cout << Max << endl;
+
+    int Min = vecMin(v);
+    cout << Min << endl;
+
+    int Count = vecCount(v, 1);
+    cout << Count << endl;
+
+    string s = "AabcBek";
+    Reverse(s);
+    cout << s;
+
+    return 0;
+}
