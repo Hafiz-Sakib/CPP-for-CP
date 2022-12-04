7d3d557192f2e3f3e379ca8d06300a3af83b1b23
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-12-04 09:48:21 +0600

    Monk And Champions League  Hackerearth Solve

--- STL/STL Codes/Priority Queue/Problem's On Priority Queue/Monk And Champions League.cpp
+++ STL/STL Codes/Priority Queue/Problem's On Priority Queue/Monk And Champions League.cpp
@@ -16,6 +16,39 @@ using namespace std;
 int main()
 {
     Boost;
+    int m, n;
+    cin >> m >> n;
+
+    priority_queue<long long> q;
+
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+
+    long long ans = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!q.empty())
+        {
+            int a = q.top();
+            q.pop();
+
+            ans += a;
+
+            a--;
+
+            if (a > 0)
+            {
+                q.push(a);
+            }
+        }
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
