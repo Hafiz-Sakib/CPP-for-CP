23770ece67ade8ae50ac96e5cc46c14c70b6c816
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-02-23 22:57:59 +0600

    Kadanes Algorithm For Maximum Subarray

---
+++ Basic Algorithms/KadanesAlgo.cpp
@@ -0,0 +1,53 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define YES cout << "Yes\n"
+#define NO cout << "No\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+int main()
+{
+    Boost;
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int maxi = v[0], sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += v[i];
+
+        maxi = max(maxi, sum);
+
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+
+    cout << maxi << endl;
+
+    return 0;
+}
