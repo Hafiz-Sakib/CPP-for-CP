0f4cbd71072b464013a15b7e525223fca8faa99e
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-03-18 22:06:28 +0600

    Trailing Zeros

---
+++ Learn Number Theory/Theory/stringTOint.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+int main()
+{
+    Boost;
+    int num = 123;
+    string s = to_string(num);
+    cout << s;
+
+    return 0;
+}
