3ec2d9393b2aa31168d88c31c0e63d9846c1d56f
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-04-20 10:38:18 +0600

    Binary Search

---
+++ CP/L1/C-15(Binary Search)/Binary_Search.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    int q;
+    cin >> q;
+
+    int first = v[0];
+    int last = v[n - 1];
+
+    for (int i = 0; i < n; i++)
+    {
+        int mid = last - first / 2;
+        if (q == v[mid])
+        {
+        }
+        else if (q > v[mid])
+        {
+            first = mid + 1;
+        }
+        else if (q < v[mid])
+        {
+            last = mid - 1;
+        }
+    }
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
