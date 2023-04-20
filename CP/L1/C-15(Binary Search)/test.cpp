d5b629d25c0bc2854b96b36499c4bcdcaf09d304
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-04-20 11:59:26 +0600

    Binary Search

--- CP/L1/C-15(Binary Search)/test.cpp
+++ CP/L1/C-15(Binary Search)/test.cpp
@@ -1,60 +1,88 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-void Boom()
-{
-    int n, t;
-    cin >> n >> t;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> v[i];
-    }
-
-    sort(v.begin(), v.end());
-
-    while (t--)
-    {
-        int first = 0;
-        int last = n - 1;
-
-        int q;
-        cin >> q;
-
-        while (first <= last)
-        {
-            int mid = (first + last) / 2;
-            if (q == v[mid])
-            {
-                cout << "found" << endl;
-                break;
-            }
-            else if (q > v[mid])
-            {
-                first = mid + 1;
-            }
-            else
-            {
-                last = mid - 1;
-            }
-        }
-        if (first > last)
-        {
-            cout << "not found" << endl;
-        }
-    }
-}
-
 int main()
 {
     Boost;
 
-    Boom();
+    class Solution
+    {
+    public:
+        int search(vector<int> &nums, int target)
+        {
+            int l = 0, r = nums.size() - 1;
+            while (l <= r)
+            {
+                int mid = (l + r) / 2;
+                if (target == nums[mid])
+                {
+                    return mid;
+                }
+                else if (target < nums[mid])
+                {
+                    r = mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            return -1;
+        }
+    };
 
     return 0;
 }
