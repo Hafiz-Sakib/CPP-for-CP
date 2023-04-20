63ffeda9c278c69dc87130e72a68ddb3e916426b
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-04-20 13:09:26 +0600

    intersection of two array

--- CP/L1/C-15(Binary Search)/leetcode349.cpp
+++ CP/L1/C-15(Binary Search)/leetcode349.cpp
@@ -56,15 +56,38 @@ typedef set<string> ss;
 
 void Boom()
 {
-    vector<int> v = {1, 2, 3};
-    cout << v.size() << endl;
+    class Solution
+    {
+    public:
+        vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+        {
+            sort(nums2.begin(), nums2.end());
+            vector<int> ret;
+            for (auto u : nums1)
+            {
+                if (binary_search(nums2.begin(), nums2.end(), u))
+                {
+                    ret.push_back(u);
+                }
+            }
+
+            sort(ret.begin(), ret.end());
+
+            int size = unique(ret.begin(), ret.end()) - ret.begin();
+
+            while (ret.size() > size)
+            {
+                ret.pop_back();
+            }
+
+            return ret;
+        }
+    };
 }
 
 int main()
 {
     Boost;
-
     Boom();
-
     return 0;
 }
