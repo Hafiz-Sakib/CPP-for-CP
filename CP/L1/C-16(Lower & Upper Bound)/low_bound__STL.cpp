122b77759ecf72f980b452a7e60808d2c00b7688
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-04-29 10:52:05 +0600

    binary search implemantation

---
+++ CP/L1/C-16(Lower & Upper Bound)/low_bound__STL.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+void Boom()
+{
+    vector<int> v = {1, 2, 2, 2, 2, 2, 3, 3, 4};
+    int x = lower_bound(v.begin(), v.end(), 3) - v.begin();
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
