46046d3870fd1ea7ffad14b54c387dd9de79a3f1
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-04-29 12:35:28 +0600

    SPOJ Binary Search Solution

--- CP/L1/C-16(Lower & Upper Bound)/Problem Solving/spoj-bsearch.cpp
+++ CP/L1/C-16(Lower & Upper Bound)/Problem Solving/spoj-bsearch.cpp
@@ -1,19 +1,5 @@
 // https://www.spoj.com/problems/BSEARCH1/
 
-/*
-Problem Statement
-You are given a sorted array of numbers, and followed by number of queries, for each query if the queried number is present in the array print its position, else print -1.
-
-Input
-First line contains N Q, number of elements in the array and number of queries to follow,
-
-Second line contains N numbers, elements of the array, each number will be -10^9<= ai <= 10^9, 0 < N <= 10^5, 0 < Q <= 5*10^5
-
-Output
-For each element in the query, print the elements 0 based location of its first occurence, if present, otherwise print -1.
-
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
 
