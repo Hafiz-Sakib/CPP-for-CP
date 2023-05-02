7e9e311e3341b3c03f7a09f0f1e84dfa07cda0df
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-02 21:40:21 +0600

    Squre Root of a number using fractional bisection

--- CP/L1/C-19(Fractional Bisection)/Problem Solving/Count_Perfect_Squares.cpp
+++ CP/L1/C-19(Fractional Bisection)/Problem Solving/Count_Perfect_Squares.cpp
@@ -22,8 +22,9 @@ const double eps = 1e-12;
 
 double perfectsqure(int n)
 {
+    n--;
     double l = 0, r = n, mid;
-    while (r - l > eps)
+    for (int i = 1; i <= 100; i++)
     {
         mid = (l + r) / 2.0;
         if (mid * mid > n)
