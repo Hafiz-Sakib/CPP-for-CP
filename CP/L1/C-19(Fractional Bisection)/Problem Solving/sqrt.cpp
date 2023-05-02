7572f1d37b268080d839abf79662cf3320fb218e
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-02 20:28:44 +0600

    Squre Root of a number using fractional bisection

---
+++ CP/L1/C-19(Fractional Bisection)/Problem Solving/sqrt.cpp
@@ -0,0 +1,42 @@
+// https://www.geeksforgeeks.org/square-root-of-an-integer/
+
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+const double eps = 1e12;
+
+double sqroot(int n)
+{
+    int l = 0, r = n, mid;
+    while (r - l < eps)
+    {
+    }
+}
+
+void Boom()
+{
+}
+
+int main()
+{
+    Boost;
+
+    Boom();
+
+    return 0;
+}
