bd785c20abc6302b79c598834e39444734069625
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-04 00:46:55 +0600

    Order Of Key PBDS Function

--- CP/L1/C-21(Policy Based Data Structures)/Theory/order_of_key.cpp
+++ CP/L1/C-21(Policy Based Data Structures)/Theory/order_of_key.cpp
@@ -7,16 +7,6 @@ using namespace std;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
              tree_order_statistics_node_update>
     ordered_set;
-typedef tree<int, null_type, greater<int>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    desending_set;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    multi_set;
-
-typedef long long int ll;
-const double eps = 1e-12;
-const int mx = 1e8 + 123;
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
