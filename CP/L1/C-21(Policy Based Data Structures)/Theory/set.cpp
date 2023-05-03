5015df4dea90bd7ca5ac734f3ab97eec84cf31cf
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-04 00:36:05 +0600

    Policy Based Data Structure Template

--- CP/L1/C-21(Policy Based Data Structures)/Theory/set.cpp
+++ CP/L1/C-21(Policy Based Data Structures)/Theory/set.cpp
@@ -7,6 +7,12 @@ using namespace std;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
              tree_order_statistics_node_update>
     ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    desending_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    multi_set;
 
 typedef long long int ll;
 const double eps = 1e-12;
