b6f5e7fd5553ad02d07dde3a78b71b4ee4e336ae
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-04 00:32:47 +0600

    Policy Based Data Structure Template

---
+++ CP/L1/C-21(Policy Based Data Structures)/Theory/pbds_template.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+
+typedef long long int ll;
+const double eps = 1e-12;
+const int mx = 1e8 + 123;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    ordered_set s;
+    s.insert(4);
+    s.insert(3);
+    s.insert(4);
+    s.insert(2);
+    s.insert(2);
+    for (auto u : s)
+    {
+        cout << u << " ";
+    }
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
