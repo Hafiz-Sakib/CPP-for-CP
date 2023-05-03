43c67f11a72dbb7c1447bf3df467b2d06ca5b8b9
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-03 11:02:25 +0600

    "Expanding Rods" LightOJ Solved

---
+++ CP/L1/C-20(More Problems on Fractional Bisection)/Problem Solving/Expanding_Rods.cpp
@@ -0,0 +1,44 @@
+// https://lightoj.com/problem/crossed-ladders
+
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef vector<ll> vi;
+
+const double eps = 1e-12;
+const int mx = 1e8 + 123;
+#define setp(n) fixed << setprecision(n)
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom(int t)
+{
+
+    //  cout << setp(10) << "Case " << t << ": " << l << endl;
+}
+
+int main()
+{
+    Boost;
+
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        Boom(i);
+    }
+
+    return 0;
+}
