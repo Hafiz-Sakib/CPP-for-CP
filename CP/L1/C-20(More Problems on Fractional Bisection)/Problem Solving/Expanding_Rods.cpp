ac654437291f2787b281b967ce599b1144ae9130
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-03 13:21:32 +0600

    "Expanding Rods" LightOJ Solved

--- CP/L1/C-20(More Problems on Fractional Bisection)/Problem Solving/Expanding_Rods.cpp
+++ CP/L1/C-20(More Problems on Fractional Bisection)/Problem Solving/Expanding_Rods.cpp
@@ -16,6 +16,7 @@ typedef vector<ll> vi;
 
 const double eps = 1e-12;
 const int mx = 1e8 + 123;
+#define PI acos(-1)
 #define setp(n) fixed << setprecision(n)
 
 #define Boost                         \
@@ -25,8 +26,32 @@ const int mx = 1e8 + 123;
 
 void Boom(int t)
 {
+    double L, n, c, cL;
+    cin >> L >> n >> c;
 
-    //  cout << setp(10) << "Case " << t << ": " << l << endl;
+    cL = (1 + (n * c)) * L;
+
+    double l = 0, r = 1e18, R, h, testing_cL;
+
+    for (int i = 0; i < 100; i++)
+    {
+        R = (l + r) / 2.0;
+
+        testing_cL = R * (2.0 * asin(L / (2.0 * R)));
+
+        if (testing_cL > cL)
+        {
+            l = R;
+        }
+        else
+        {
+            r = R;
+        }
+    }
+
+    h = R - sqrt((R * R) - ((L / 2.0) * (L / 2.0)));
+
+    cout << setp(10) << "Case " << t << ": " << h << endl;
 }
 
 int main()
