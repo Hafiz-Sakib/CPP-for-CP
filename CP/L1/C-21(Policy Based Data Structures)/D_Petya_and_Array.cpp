f6e8d88317d16e114c2a3087da63fc9bff98b0a5
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-04 20:03:16 +0600

    Petya and Array Codeforces Solution

---
+++ CP/L1/C-21(Policy Based Data Structures)/D_Petya_and_Array.cpp
@@ -0,0 +1,62 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<string> vs;
+
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+#define setp(n) fixed << setprecision(n)
+
+const double eps = 1e-12;
+const int mx = 1e8 + 123;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
