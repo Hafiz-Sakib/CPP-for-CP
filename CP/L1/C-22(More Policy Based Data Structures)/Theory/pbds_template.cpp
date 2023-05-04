be43ab9f023752e22d1d4a6f581b5620c4aca439
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-05 01:43:10 +0600

    Policy Based Data Structures

--- CP/L1/C-22(More Policy Based Data Structures)/Theory/pbds_template.cpp
+++ CP/L1/C-22(More Policy Based Data Structures)/Theory/pbds_template.cpp
@@ -18,6 +18,9 @@ typedef tree<ll, null_type, greater_equal<ll>, rb_tree_tag,
              tree_order_statistics_node_update>
     greater_multi_set;
 
+// cout<<*X.find_by_order(1)<<endl; // iterator to the k-th largest element
+// cout<<X.order_of_key(-5)<<endl;  // number of items in a set that are strictly smaller than our item
+
 typedef long long int ll;
 const double eps = 1e-12;
 const int mx = 1e8 + 123;
