b0e67fecac977a3796762554944cad5e95165727
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-05 21:11:34 +0600

    A. Subtle Substring Subtraction

--- CP/L1/C-22(More Policy Based Data Structures)/A_Subtle_Substring_Subtraction.cpp
+++ CP/L1/C-22(More Policy Based Data Structures)/A_Subtle_Substring_Subtraction.cpp
@@ -1,35 +1,16 @@
+// https://codeforces.com/contest/1673/problem/A
+
 // Bismillahir Rahmanir Rahim
+
 /*
 
 string author;
 author = Hafiz_Sakib;
 
 */
+
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long int ll;
-typedef unsigned long long ull;
-
-typedef vector<ll> vi;
-typedef vector<string> vs;
-
-#define space ' '
-#define endl "\n"
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
-#define b() begin()
-#define e() end()
-#define all(data) data.begin(), data.end()
-#define rall(data) data.rbegin(), data.rend()
-
-#define debug(x) cerr << x << endl;
-#define here fprintf(stderr, "====I am Here====\n");
-#define setp(n) fixed << setprecision(n)
-
-const double eps = 1e-12;
-const int mx = 1e8 + 123;
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
@@ -38,7 +19,7 @@ const int mx = 1e8 + 123;
 
 int sum(string s)
 {
-    ll ans = 0;
+    int ans = 0;
     for (auto u : s)
     {
         ans += (u - 'a' + 1);
