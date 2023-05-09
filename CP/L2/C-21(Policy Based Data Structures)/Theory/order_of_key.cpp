8441db0975cb9cf793d4a189215501d540db292e
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-09 09:40:45 +0600

    Policy Based Data Structures Special Functions

--- CP/L2/C-21(Policy Based Data Structures)/Theory/order_of_key.cpp
+++ CP/L2/C-21(Policy Based Data Structures)/Theory/order_of_key.cpp
@@ -1,12 +1,55 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 using namespace std;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<string> vs;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
              tree_order_statistics_node_update>
     ordered_set;
+typedef tree<ll, null_type, greater<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    greater_set;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    multi_set;
+typedef tree<ll, null_type, greater_equal<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    greater_multi_set;
+
+#define space ' '
+#define endl "\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define YES cout << "Yes\n"
+#define NO cout << "No\n"
+
+#
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
@@ -16,22 +59,24 @@ typedef tree<int, null_type, less<int>, rb_tree_tag,
 void Boom()
 {
     ordered_set s;
+
+    s.insert(2);
     s.insert(4);
+    s.insert(2);
+    s.insert(1);
+    s.insert(8);
+    s.insert(7);
     s.insert(3);
-    s.insert(4);
-    s.insert(2);
-    s.insert(2);
 
-    cout << s.size() << endl;
+    // order_of_key(x) – ফাংশনটি x এলিমেন্টটা কোন পজিশনে আছে সেটা বলে দেয়।
 
-    for (auto u : s)
-    {
-        cout << u << " ";
-    }
+    // অথবা,x এর চাইতে কয়টি ছোট এলিমেন্ট আছে সেটা  বলে দেয়।
+
+    // set_name.order_of_key(x)
 
     cout << s.order_of_key(3) << endl;
 
-    // ৩ এর চাইতে কয়টা ছোট ভ্যালু আছে সেটা রিটার্ন করবে এই ফাংশন !
+    // ৩ এর চাইতে কয়টা ছোট ভ্যালু আছে অথবা ৩ এর ইন্ডেক্স কত সেটা রিটার্ন করবে এই ফাংশন !
 }
 
 int main()
