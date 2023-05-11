0cd34d9c9c8985696e885e658d02469ec5656dfb
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-11 19:46:41 +0600

    Divisors Of  Number

--- CP/L1/C-25(Number Theory - Sieve of Eratosthenes)/previous.cpp
+++ CP/L1/C-25(Number Theory - Sieve of Eratosthenes)/previous.cpp
@@ -12,8 +12,23 @@ typedef long long int ll;
 typedef unsigned long long ull;
 
 typedef vector<ll> vi;
+typedef vector<double> vd;
 typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<string> ss;
 
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
 #define space ' '
 #define endl "\n"
 #define yes cout << "YES\n"
@@ -23,35 +38,50 @@ typedef vector<string> vs;
 #define e() end()
 #define all(data) data.begin(), data.end()
 #define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
 
-#define debug(x) cerr << x << endl;
-#define here fprintf(stderr, "====I am Here====\n");
-#define setp(n) fixed << setprecision(n)
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
 
 const double eps = 1e-12;
 const int mx = 1e8 + 123;
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
+int cnt[mx];
+
 void Boom()
 {
+
     int n;
     cin >> n;
-    vector<int> v(n + 1);
+    vvi v(n + 1);
 
-    for (int i = 1; i * i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        if (n % i)
+        for (int j = i; j <= n; j += i)
         {
-            v.push_back(i);
-            if (n / i != i)
-            {
-                v.push_back(n / i);
-            }
+            cnt[j]++;
         }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " : ";
+        for (auto u : v[i])
+        {
+            cout << u << ' ';
+        }
+        cout << endl;
     }
 }
 
