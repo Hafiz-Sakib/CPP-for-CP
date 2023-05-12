bb9680a64f0cbcc2dba35a6d59f021fc9d2b86de
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-12 12:25:30 +0600

    Printing Primes Till 1e8 SPOJ

--- CP/L1/C-25(Number Theory - Sieve of Eratosthenes)/Theory/Printing_some_primes_1e8.cpp
+++ CP/L1/C-25(Number Theory - Sieve of Eratosthenes)/Theory/Printing_some_primes_1e8.cpp
@@ -2,27 +2,36 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+const int mx = 1e8;
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-vector<bool> sieve(int n)
+vector<bool> is_prime(mx, 1);
+vector<bool> primes;
+
+void sieve(int n)
 {
-    vector<bool> is_prime(n + 1, true);
-    is_prime[0] = is_prime[1] = false;
+    is_prime[1] = 0;
     for (int i = 2; i * i <= n; i++)
     {
         if (is_prime[i])
         {
             for (int j = i * i; j <= n; j += i)
             {
-                is_prime[j] = false;
+                is_prime[j] = 0;
             }
         }
     }
-    return is_prime;
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (is_prime[i])
+        {
+            primes.push_back(i);
+        }
+    }
 }
 
 int main()
@@ -30,7 +39,6 @@ int main()
     Boost;
 
     int n = 1e8;
-    vector<bool> primes = sieve(n);
     int count = 0;
     for (int i = 2; i <= n; i++)
     {
