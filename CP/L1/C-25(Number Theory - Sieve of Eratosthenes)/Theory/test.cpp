04e96cbaa99d13d5bcadc9b95dc36fe882f75f55
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-12 10:42:59 +0600

    Sieve of Eratosthenes

--- CP/L1/C-25(Number Theory - Sieve of Eratosthenes)/Theory/test.cpp
+++ CP/L1/C-25(Number Theory - Sieve of Eratosthenes)/Theory/test.cpp
@@ -8,41 +8,33 @@ author = Hafiz_Sakib;
 #include <bits/stdc++.h>
 using namespace std;
 
+const double eps = 1e-12;
+const int mx = 1e8 + 123;
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-const int MX = 1e7 + 123;
-bitset<MX> is_prime;
+const int MX = 1e8;
+// bitset<MX> is_prime;
 vector<int> prime;
+vector<bool> is_prime(mx, 1);
 
-void primeGen(int n)
+void sieve(int n)
 {
-    n += 100;
-    for (int i = 3; i <= n; i += 2)
-    {
-        is_prime[i] = 1;
-    }
+    is_prime[1] = 0;
 
-    int sq = sqrt(n);
-    for (int i = 3; i <= sq; i += 2)
+    for (int i = 2; i <= n; i++)
     {
-        if (is_prime[i] == 1)
+        for (int j = i + i; i <= n; j += i)
         {
-            for (int j = i * i; j <= n; j += (i + i))
-            {
-                is_prime[j] = 0;
-            }
+            is_prime[j] = 0;
         }
     }
 
-    is_prime[2] = 1;
-    prime.push_back(2);
-
-    for (int i = 3; i <= n; i += 2)
+    for (int i = 1; i <= n; i++)
     {
-        if (is_prime[i] == 1)
+        if (is_prime[i])
         {
             prime.push_back(i);
         }
