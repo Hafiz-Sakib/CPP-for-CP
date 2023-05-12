c27cdd814c2a5fff3ddb3c599d67e2c0c4c95e78
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-12 11:44:18 +0600

    Sieve of Eratosthenes

--- CP/L1/C-25(Number Theory - Sieve of Eratosthenes)/Theory/test.cpp
+++ CP/L1/C-25(Number Theory - Sieve of Eratosthenes)/Theory/test.cpp
@@ -17,20 +17,30 @@ const int mx = 1e8 + 123;
 
 const int MX = 1e8;
 // bitset<MX> is_prime;
+vector<bool> is_prime(mx);
 vector<int> prime;
-vector<bool> is_prime(mx, 1);
 
 void sieve(int n)
 {
     is_prime[1] = 0;
-
-    for (int i = 2; i <= n; i++)
+    for (int i = 3; i <= n; i += 2)
     {
-        for (int j = i + i; i <= n; j += i)
+        is_prime[i] = 1;
+    }
+
+    for (int i = 3; (i * i) <= n; i += 2)
+    {
+        if (is_prime[i])
         {
-            is_prime[j] = 0;
+            for (int j = (i * i); i <= n; j += (i + i))
+            {
+                is_prime[j] = 0;
+            }
         }
     }
+
+    is_prime[2] = 1;
+    prime.push_back(2);
 
     for (int i = 1; i <= n; i++)
     {
@@ -40,9 +50,16 @@ void sieve(int n)
         }
     }
 }
-
 int main()
 {
     Boost;
+    sieve(25);
+    for (auto u : prime)
+    {
+        cout << u << ' ';
+    }
+
+    cout << endl;
+
     return 0;
 }
