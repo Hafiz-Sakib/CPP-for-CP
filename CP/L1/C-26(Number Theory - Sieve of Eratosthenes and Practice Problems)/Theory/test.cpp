535118f6aac967bfc18b67bc7c9d8301393035bd
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-13 12:07:51 +0600

    Sieve of Eratosthenes

--- CP/L1/C-26(Number Theory - Sieve of Eratosthenes and Practice Problems)/Theory/test.cpp
+++ CP/L1/C-26(Number Theory - Sieve of Eratosthenes and Practice Problems)/Theory/test.cpp
@@ -1,5 +1,3 @@
-// https://www.spoj.com/problems/TDPRIMES/
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,18 +6,20 @@ using namespace std;
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-const int mx = 1e8;
-
+const int mx = 1e8 + 123;
 bitset<mx> is_prime;
 vector<int> primes;
-void sieve(int n)
+
+void Sieve(int n)
 {
+    is_prime[1] = 0;
+
     for (int i = 3; i <= n; i += 2)
     {
         is_prime[i] = 1;
     }
 
-    for (int i = 3; (i * i) <= n; i += 2)
+    for (int i = 3; i * i <= n; i += 2)
     {
         if (is_prime[i])
         {
@@ -30,6 +30,7 @@ void sieve(int n)
         }
     }
 
+    is_prime[2] = 1;
     primes.push_back(2);
 
     for (int i = 3; i <= n; i += 2)
@@ -44,7 +45,7 @@ void sieve(int n)
 int main()
 {
     Boost;
-    sieve(mx);
+    Sieve(1e8);
     int c = 0;
     for (int i = 0; i < primes.size(); i++)
     {
@@ -52,6 +53,10 @@ int main()
         if (c % 100 == 1)
         {
             cout << primes[i] << endl;
+        }
+        else
+        {
+            continue;
         }
     }
 
