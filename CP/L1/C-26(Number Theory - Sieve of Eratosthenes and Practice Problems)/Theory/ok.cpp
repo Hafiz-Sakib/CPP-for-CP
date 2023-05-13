4f8d1a5c103bc6ef7542323c72d183a9aa3e045c
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-13 14:53:46 +0600

    Sieve of Eratosthenes

--- CP/L1/C-26(Number Theory - Sieve of Eratosthenes and Practice Problems)/Theory/ok.cpp
+++ CP/L1/C-26(Number Theory - Sieve of Eratosthenes and Practice Problems)/Theory/ok.cpp
@@ -19,85 +19,99 @@ typedef vector<vector<ll>> vvi;
 typedef vector<pair<ll, ll>> vpii;
 typedef vector<pair<string, ll>> vpsi;
 typedef pair<ll, ll> pii;
+typedef pair<string, ll> psi;
+typedef pair<ll, string> pis;
+typedef pair<double, double> pdd;
 typedef map<ll, ll> mii;
+typedef map<ll, bool> mib;
 typedef map<string, ll> msi;
+typedef map<ll, string> mis;
 typedef set<ll> si;
 typedef set<string> ss;
+typedef set<double> sd;
+typedef priority_queue<ll> pq;
+typedef priority_queue<string> pqs;
+typedef priority_queue<ll, vi, greater<ll>> pqg;
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
 
 #define pb push_back
 #define mp make_pair
+#define pob pop_back
 #define in insert
 #define fi first
 #define se second
 #define space ' '
 #define endl "\n"
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
+#define lb lower_bound
+#define ub upper_bound
+
+#define left(node) (node * 2)
+#define mid(l, r) ((r + l) >> 1)
+#define right(node) (node * 2 + 1)
 
 #define b() begin()
 #define e() end()
+#define sz(x) (int)x.size()
 #define all(data) data.begin(), data.end()
 #define rall(data) data.rbegin(), data.rend()
+#define Reverse(data) reverse(data.begin(), data.end())
 #define vecMax(data) *max_element(data.begin(), data.end())
 #define vecMin(data) *min_element(data.begin(), data.end())
 #define vecSum(data) accumulate(data.begin(), data.end(), 0)
+#define vecCount(data, key) count(data.begin(), data.end(), key)
 
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define YES cout << "Yes\n"
+#define NO cout << "No\n"
+
+#define setp(n) fixed << setprecision(n)
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define mem(arr, val) memset(arr, val, sizeof(arr));
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
 #define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
 #define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
-#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
-
-const double eps = 1e-12;
-const int mx = 1e8 + 123;
 
 #define debug(x) cerr << x << endl;
 #define here fprintf(stderr, "====I am Here====\n");
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define sqr(a) ((a) * (a))
+#define MOD 1000000007
+#define mx_int_prime 999999937
+
+const double PI = acos(-1);
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-bitset<mx> is_prime;
-vector<int> primes;
-
-void sieve(int n)
+void Boom()
 {
-    n += 100;
-
-    for (int i = 3; i <= n; i += 2)
-    {
-        is_prime[i] = 1;
-    }
-
-    for (int i = 3; (i * i) <= n; i += 2)
-    {
-        if (is_prime[i])
-        {
-            for (int j = (i * i); j <= n; j += (i + i))
-            {
-                is_prime[j] = 0;
-            }
-        }
-    }
-
-    is_prime[1] = 0;
-    is_prime[2] = 1;
-    primes.push_back(2);
-
-    for (int i = 3; i <= n; i += 2)
-    {
-        if (is_prime[i])
-        {
-            primes.push_back(i);
-        }
-    }
+    // Let's Move
 }
 
 int main()
 {
     Boost;
 
-    sieve(1e8);
-    cout << primes.size() << ' ' << primes[124545] << endl;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // cout<<"Case "<<t<<" : ";
+        Boom();
+    }
+
     return 0;
 }
