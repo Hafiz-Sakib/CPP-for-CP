3df3ca851f2c867d675b35724fa4563f8052c11d
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-14 13:54:44 +0600

    Prime Factorization

---
+++ CP/L1/C-27(Prime Factorization and Practise Problems)/Problem Solving/Largest_Prime_Divisor_Without_Sieve.cpp
@@ -0,0 +1,85 @@
+// https://vjudge.net/problem/UVA-583
+
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+const int mx = 1e8 + 123;
+#define pb push_back
+#define endl "\n"
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+vector<int> PrimeFactors;
+
+void PrimeFactorization(int n)
+{
+    PrimeFactors.clear();
+
+    while (n % 2 == 0)
+    {
+        PrimeFactors.pb(2);
+        n = n / 2;
+    }
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        while (n % i == 0)
+        {
+            PrimeFactors.pb(i);
+            n = n / i;
+        }
+    }
+    if (n > 2)
+    {
+        PrimeFactors.pb(n);
+    }
+}
+void Boom()
+{
+    ll n;
+    while (1)
+    {
+        int c = 0;
+        cin >> n;
+        if (!n)
+        {
+            break;
+        }
+
+        PrimeFactorization(abs(n));
+
+        cout << n << " = ";
+
+        if (n < 0)
+        {
+            cout << "-1 x ";
+        }
+        for (auto u : PrimeFactors)
+        {
+            c++;
+            cout << u;
+            if (c < PrimeFactors.size())
+            {
+                cout << " x ";
+            }
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
