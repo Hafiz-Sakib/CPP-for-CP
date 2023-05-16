edfe9442c3e9a0b09cbb5dd77f4421ac19853746
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-16 20:49:05 +0600

    GCD and LCM

---
+++ CP/L1/C-28 (Number Theory - Prime Factorization and Practice Problems)/tempCodeRunnerFile.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+int GCD(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    if (a < b)
+    {
+        swap(a, b);
+    }
+    int r = a % b;
+    return GCD(b, r);
+}
+int LCM(int a, int b)
+{
+    int m = abs(a * b);
+    return m / GCD(a, b);
+}
+void Boom()
+{
+    int a = 12, b = 6;
+    cout << GCD(a, b) << endl;
+    cout << LCM(a, b) << endl;
+}
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
