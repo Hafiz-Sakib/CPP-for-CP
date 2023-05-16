1138e5fbd9d5341e24f5a0bc1ce228dbcef1c1bb
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-16 10:32:45 +0600

    Folder Re-Organized

---
+++ CP/L1/C-28 (Number Theory - Prime Factorization and Practice Problems)/test.java
@@ -0,0 +1,56 @@
+class Car {
+    private String make;
+    private String model;
+    private int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
+
+public class test {
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Corolla", 2022);
+
+        System.out.println("Make: " + myCar.getMake());
+        System.out.println("Model: " + myCar.getModel());
+        System.out.println("Year: " + myCar.getYear());
+
+        myCar.setMake("Honda");
+        myCar.setModel("Accord");
+        myCar.setYear(2023);
+        myCar.displayInfo();
+    }
+}
