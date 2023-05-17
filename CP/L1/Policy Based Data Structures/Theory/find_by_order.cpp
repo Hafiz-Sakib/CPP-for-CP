753abb980ece68bd86bdf888e6e7400f89cf9bee
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-17 21:04:59 +0600

    Find By Order

--- CP/L1/Policy Based Data Structures/Theory/find_by_order.cpp
+++ CP/L1/Policy Based Data Structures/Theory/find_by_order.cpp
@@ -1,10 +1,3 @@
-// Bismillahir Rahmanir Rahim
-/*
-
-string author;
-author = Hafiz_Sakib;
-
-*/
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -12,44 +5,10 @@ using namespace __gnu_pbds;
 using namespace std;
 
 typedef long long int ll;
-typedef unsigned long long ull;
 
-typedef vector<ll> vi;
-typedef vector<string> vs;
-typedef pair<ll, ll> pii;
-typedef map<ll, ll> mii;
-typedef map<string, ll> msi;
-
-typedef vector<int>::iterator vit;
-typedef set<int>::iterator sit;
-
-typedef tree<ll, null_type, less<ll>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    ordered_set;
-typedef tree<ll, null_type, greater<ll>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    greater_set;
 typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag,
              tree_order_statistics_node_update>
     multi_set;
-typedef tree<ll, null_type, greater_equal<ll>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    greater_multi_set;
-
-#define space ' '
-#define endl "\n"
-
-#define b() begin()
-#define e() end()
-#define all(data) data.begin(), data.end()
-#define rall(data) data.rbegin(), data.rend()
-
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-#define YES cout << "Yes\n"
-#define NO cout << "No\n"
-
-#
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
