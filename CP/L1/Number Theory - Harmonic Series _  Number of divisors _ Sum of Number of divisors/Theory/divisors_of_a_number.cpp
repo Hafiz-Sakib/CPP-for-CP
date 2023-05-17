b07af4444d47a16958f474224288618eb955d6e1
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-17 21:00:14 +0600

    Find Divisors of  a number

--- CP/L1/Number Theory - Harmonic Series _  Number of divisors _ Sum of Number of divisors/Theory/divisors_of_a_number.cpp
+++ CP/L1/Number Theory - Harmonic Series _  Number of divisors _ Sum of Number of divisors/Theory/divisors_of_a_number.cpp
@@ -1,35 +1,7 @@
-// Bismillahir Rahmanir Rahim
-/*
+// Find Divisors of a Number Using efficient Way
 
-string author;
-author = Hafiz_Sakib;
-
-*/
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long int ll;
-typedef unsigned long long ull;
-
-typedef vector<ll> vi;
-typedef vector<string> vs;
-
-#define space ' '
-#define endl "\n"
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
-#define b() begin()
-#define e() end()
-#define all(data) data.begin(), data.end()
-#define rall(data) data.rbegin(), data.rend()
-
-#define debug(x) cerr << x << endl;
-#define here fprintf(stderr, "====I am Here====\n");
-#define setp(n) fixed << setprecision(n)
-
-const double eps = 1e-12;
-const int mx = 1e8 + 123;
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
@@ -54,7 +26,7 @@ void Boom()
         }
     }
 
-    sort(all(divisors));
+    sort(divisors.begin(), divisors.end());
 
     for (auto u : divisors)
     {
