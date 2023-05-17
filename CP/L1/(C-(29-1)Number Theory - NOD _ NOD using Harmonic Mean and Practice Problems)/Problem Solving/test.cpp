174e7fc675e8e873db950c1f924aabaad52e7608
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-17 23:04:18 +0600

    NOD Using Harmonic Mean

---
+++ CP/L1/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/Problem Solving/test.cpp
@@ -0,0 +1,29 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef vector<ll> vi;
+const int mx = 1e8 + 123;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
