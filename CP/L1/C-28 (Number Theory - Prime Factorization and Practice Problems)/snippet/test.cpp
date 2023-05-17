d842b29de5565d6a4b73671065e351357957e773
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-17 20:39:16 +0600

    test

---
+++ CP/L1/C-28 (Number Theory - Prime Factorization and Practice Problems)/snippet/test.cpp
@@ -0,0 +1,193 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ull unsigned long long int
+
+#define vi vector<ll>
+#define vd vector<double>
+#define vs vector<string>
+#define vc vector<char>
+#define vvi vector<vector<ll>>
+#define vpii vector<pair<ll, ll>>
+#define vpsi vector<pair<string, ll>>
+#define pii pair<ll, ll>
+#define psi pair<string, ll>
+#define pis pair<ll, string>
+#define mii map<ll, ll>
+#define mib map<ll, bool>
+#define msi map<string, ll>
+#define mis map<ll, string>
+#define pdd pair<double, double>
+#define si set<ll>
+#define sc set<char>
+#define ss set<string>
+#define sd set<double>
+#define pq priority_queue<ll>
+#define pqs priority_queue<string>
+#define pqg priority_queue<ll, vi, greater<ll>>
+
+#define vit vector<int>::iterator
+#define sit set<int>::iterator
+
+#define pb push_back
+#define mp make_pair
+#define pob pop_back
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define lb lower_bound
+#define ub upper_bound
+
+#define left(node) (node * 2)
+#define mid(l, r) ((r + l) >> 1)
+#define right(node) (node * 2 + 1)
+
+#define b() begin()
+#define e() end()
+#define sz(x) (int)x.size()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define Reverse(data) reverse(data.begin(), data.end())
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+#define vecCount(data, key) count(data.begin(), data.end(), key)
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define YES cout << "Yes\n"
+#define NO cout << "No\n"
+
+#define setp(n) fixed << setprecision(n)
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define mem(arr, val) memset(arr, val, sizeof(arr));
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define sqr(a) ((a) * (a))
+#define MOD 1000000007
+#define mx_int_prime 999999937
+
+const double PI = acos(-1);
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+bitset<mx> is_prime;
+vector<int> primes;
+
+void sieve(int n)
+{
+    for (int i = 3; i <= n; i += 2)
+    {
+        is_prime[i] = 1;
+    }
+
+    for (int i = 3; (i * i) <= n; i += 2)
+    {
+        if (is_prime[i])
+        {
+            for (int j = (i * i); j <= n; j += (i + i))
+            {
+                is_prime[j] = 0;
+            }
+        }
+    }
+
+    is_prime[1] = 0;
+    is_prime[2] = 1;
+    primes.push_back(2);
+
+    for (int i = 3; i <= n; i += 2)
+    {
+        if (is_prime[i])
+        {
+            primes.push_back(i);
+        }
+    }
+}
+
+vector<ll> PrimeFactors;
+
+void PrimeFactorization(ll n)
+{
+    PrimeFactors.clear();
+    // Call sieve function first for access primes till n
+    for (auto p : primes)
+    {
+        if (1LL * p * p > n)
+        {
+            break;
+        }
+        else if (n % p == 0)
+        {
+            while (n % p == 0)
+            {
+                PrimeFactors.push_back(p);
+                n /= p;
+            }
+        }
+    }
+    if (n > 1)
+    {
+        PrimeFactors.push_back(n);
+    }
+}
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+
+void Boom()
+{
+    // Let's Move
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // cout<<"Case "<<t<<" : ";
+        Boom();
+    }
+
+    return 0;
+}
