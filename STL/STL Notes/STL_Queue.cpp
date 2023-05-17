2ab4b98628f3eeaa5180035abe3bcbfa3938622e
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-17 22:05:38 +0600

    STL Queue

--- STL/STL Notes/STL_Queue.cpp
+++ STL/STL Notes/STL_Queue.cpp
@@ -1,22 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    queue<int>q; //Queue decleration
-    q.push(5); //Push an element in queue
+    queue<int> q;
+    // Queue decleration
+
+    // Push an element in queue
+    q.push(5);
     q.push(10);
     q.push(15);
-    q.emplace(20); //Push an element in Queue
-    cout<<q.size()<<endl; //getting size of queue
-    if(q.empty()) cout<<"Empty"<<endl; //Check if the queue is empty or not
-    else cout<<"Not Empty"<<endl;
-    cout<<q.front()<<" ... "<<q.back()<<endl; //Print first & last element
-    while(q.size()>0){
-        cout<<q.front()<<" "; //Printing Queue process
-        q.pop(); //Delete the first element
+    q.emplace(20);
+    // Push an element in Queue (emplace does it too!)
+
+    cout << q.size() << endl;
+    // getting size of queue
+
+    if (q.empty())
+    {
+        cout << "Empty" << endl;
+        // Check if the queue is empty or not
     }
-    queue<int>q2;
-    q.swap(q2); // Swap the elements of 2 queue
+    else
+    {
+        cout << "Not Empty" << endl;
+    }
+
+    cout << q.front() << " ... " << q.back() << endl;
+    // Print first & last element
+
+    while (q.size() > 0)
+    {
+        cout << q.front() << " ";
+        // Printing Queue process
+        q.pop();
+        // Delete the first element
+    }
+
+    queue<int> q2;
+    q.swap(q2);
+    // Swap the elements of 2 queue
+
     return 0;
 }
