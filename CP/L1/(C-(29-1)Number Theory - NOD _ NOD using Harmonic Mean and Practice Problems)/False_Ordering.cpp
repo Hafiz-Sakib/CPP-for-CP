652c373a5d031d8355453b0331db1c5c53f83884
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-19 01:24:37 +0600

    False Ordering

---
+++ CP/L1/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/False_Ordering.cpp
@@ -0,0 +1,39 @@
+// https://lightoj.com/problem/false-ordering
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef vector<pair<ll, ll>> vpii;
+typedef pair<ll, ll> pii;
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
