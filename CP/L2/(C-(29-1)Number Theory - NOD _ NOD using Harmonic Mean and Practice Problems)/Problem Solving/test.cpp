d4592901bcf09b24dd9480a706549a09de9d5560
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-18 20:14:51 +0600

    Code Screenshots added

--- CP/L2/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/Problem Solving/test.cpp
+++ CP/L2/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/Problem Solving/test.cpp
@@ -1,25 +1,24 @@
-// LCM of 2 Numbers
+// Bismillahir Rahmanir Rahim
+/*
 
+string author;
+author = Hafiz_Sakib;
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
+
+typedef long long int ll;
+typedef vector<ll> vi;
+const int mx = 1e8 + 123;
+
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a) / gcd(a, b) * (b))
-
-int LCM(int a, int b)
-{
-    int m = abs(a * b);
-    return m / gcd(a, b);
-}
-
 void Boom()
 {
-    int a = 12, b = 6;
-    cout << lcm(a, b) << endl;
 }
 
 int main()
