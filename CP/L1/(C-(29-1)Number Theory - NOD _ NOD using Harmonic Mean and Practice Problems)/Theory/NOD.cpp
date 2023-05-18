a31997d58429b044e59183ac9395c99f2bfc10ce
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-19 00:09:54 +0600

    NOD Function

---
+++ CP/L1/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/Theory/NOD.cpp
@@ -0,0 +1,87 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+const int mx = 1e8;
+
+bitset<mx> is_prime;
+vector<int> primes;
+
+void sieve(int n)
+{
+    for (int i = 3; i <= n; i += 2)
+    {
+        is_prime[i] = 1;
+    }
+
+    for (int i = 3; (i * i) <= n; i += 2)
+    {
+        if (is_prime[i])
+        {
+            for (int j = (i * i); j <= n; j += (i + i))
+            {
+                is_prime[j] = 0;
+            }
+        }
+    }
+
+    is_prime[1] = 0;
+    is_prime[2] = 1;
+    primes.push_back(2);
+
+    for (int i = 3; i <= n; i += 2)
+    {
+        if (is_prime[i])
+        {
+            primes.push_back(i);
+        }
+    }
+}
+
+int NOD(ll n)
+{
+    // This function will return how many divisors in (n) using Prime Factorization
+    int ans = 1;
+    // Call Sieve Function First
+    for (auto p : primes)
+    {
+        if (1LL * p * p > n)
+        {
+            break;
+        }
+        else if (n % p == 0)
+        {
+            int cnt = 1;
+            while (n % p == 0)
+            {
+                n /= p;
+                cnt++;
+            }
+            ans *= cnt;
+        }
+    }
+    if (n > 1)
+    {
+        ans *= 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    sieve(1000000);
+    int n = 6345, cnt = 0;
+
+    cout << NOD(n) << endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
