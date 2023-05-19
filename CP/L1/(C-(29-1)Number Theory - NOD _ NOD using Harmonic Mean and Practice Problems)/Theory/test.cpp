e6580542b3a22d871b56d477f1e2570becd11a0b
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-19 20:34:52 +0600

    Comparator

--- CP/L1/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/Theory/test.cpp
+++ CP/L1/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/Theory/test.cpp
@@ -1,123 +1,19 @@
-// https://lightoj.com/problem/false-ordering
-
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long int ll;
-typedef vector<ll> vi;
-typedef pair<ll, ll> pii;
-typedef vector<pair<ll, ll>> vpii;
-typedef pair<ll, ll> pii;
-
-#define b() begin()
-#define e() end()
-#define all(data) data.begin(), data.end()
-#define endl "\n"
-
-const int mx = 1e8 + 123;
-const double eps = 1e-12;
+#define here fprintf(stderr, "====I am Here====\n");
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
-    cout.tie(NULL);
+    cout.tie(NULL)
 
-bitset<mx> is_prime;
-vector<int> primes;
-
-void sieve(int n)
+void Boom()
 {
-    for (int i = 3; i <= n; i += 2)
-    {
-        is_prime[i] = 1;
-    }
-
-    for (int i = 3; (i * i) <= n; i += 2)
-    {
-        if (is_prime[i])
-        {
-            for (int j = (i * i); j <= n; j += (i + i))
-            {
-                is_prime[j] = 0;
-            }
-        }
-    }
-
-    is_prime[1] = 0;
-    is_prime[2] = 1;
-    primes.push_back(2);
-
-    for (int i = 3; i <= n; i += 2)
-    {
-        if (is_prime[i])
-        {
-            primes.push_back(i);
-        }
-    }
 }
-
-bool cmp(pii a, pii b)
-{
-    if (a.first < b.first)
-    {
-        return 1;
-    }
-    else if (a.first == b.first)
-    {
-        return a.second > b.second;
-    }
-    return 0;
-}
-
-vector<vector<int>> PrimePower(1123);
-vector<int> NOD(1123);
 
 int main()
 {
     Boost;
-    int lim = 1000;
-    sieve(1000);
-
-    for (auto p : primes)
-    {
-        for (int i = p; i <= lim; i += p)
-        {
-            int cnt = 1, tmp = i;
-            while (tmp % p == 0)
-            {
-                tmp /= p;
-                cnt++;
-            }
-            PrimePower[i].push_back(cnt);
-        }
-    }
-
-    for (int i = 1; i <= lim; i++)
-    {
-        NOD[i] = 1;
-        for (auto u : PrimePower[i])
-        {
-            NOD[i] *= u;
-        }
-    }
-
-    vector<pair<int, int>> v;
-
-    for (int i = 1; i <= lim; i++)
-    {
-        v.push_back({NOD[i], i});
-    }
-
-    sort(all(v), cmp);
-
-    int t;
-    cin >> t;
-    for (int i = 1; i <= t; i++)
-    {
-        int n;
-        cin >> n;
-        cout << "Case " << i << ": " << v[n - 1].second << endl;
-    }
-
+    Boom();
     return 0;
 }
