33e90be8ff71901c12046f4cec9d9daa09e4fd38
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-19 09:22:47 +0600

    False Ordering

--- CP/L1/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/False_Ordering.cpp
+++ CP/L1/(C-(29-1)Number Theory - NOD _ NOD using Harmonic Mean and Practice Problems)/False_Ordering.cpp
@@ -18,21 +18,90 @@ const double eps = 1e-12;
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
-    cout.tie(NULL)
+    cout.tie(NULL);
 
-void Boom()
+bitset<mx> is_prime;
+vector<int> primes;
+
+void sieve(int n)
 {
+    for (int i = 3; i <= n; i += 2)
+    {
+        is_prime[i] = 1;
+    }
+
+    for (int i = 3; (i * i) <= n; i += 2)
+    {
+        if (is_prime[i])
+        {
+            for (int j = (i * i); j <= n; j += (i + i))
+            {
+                is_prime[j] = 0;
+            }
+        }
+    }
+
+    is_prime[1] = 0;
+    is_prime[2] = 1;
+    primes.push_back(2);
+
+    for (int i = 3; i <= n; i += 2)
+    {
+        if (is_prime[i])
+        {
+            primes.push_back(i);
+        }
+    }
 }
+
+vector<vector<int>> PrimePower(1123);
+vector<int> NOD(1123);
 
 int main()
 {
     Boost;
+    int lim = 1000;
+    sieve(1000);
 
-    int t = 1;
-    cin >> t;
-    while (t--)
+    for (auto p : primes)
     {
-        Boom();
+        for (int i = p; i <= lim; i += p)
+        {
+            int cnt = 1, tmp = i;
+            while (tmp % p == 0)
+            {
+                tmp /= p;
+                cnt++;
+            }
+            PrimePower[i].push_back(cnt);
+        }
+    }
+
+    for (int i = 1; i <= lim; i++)
+    {
+        NOD[i] = 1;
+        for (auto u : PrimePower[i])
+        {
+            NOD[i] *= u;
+        }
+    }
+
+    vector<pair<int, int>> v;
+
+    for (int i = 1; i <= lim; i++)
+    {
+        v.push_back({NOD[i], -i});
+    }
+
+    sort(all(v));
+
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        int n;
+        cin >> n;
+        cout << "Case " << i << ": " << -1 * v[n - 1].second << endl;
     }
 
     return 0;
