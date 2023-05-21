ced7409e57cb4006fc6536392e41b16548edfac1
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-22 00:45:28 +0600

    Sum of Diisors Code ScreenShots Added

--- CP/L2/C -31 ( Number Theory - NOD_SOD and Practice Problems )/Theory/test.cpp
+++ CP/L2/C -31 ( Number Theory - NOD_SOD and Practice Problems )/Theory/test.cpp
@@ -1,16 +1,69 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
-const int mx = 1e8;
+
 bitset<mx> is_prime;
 vector<int> primes;
 
-void sieve(int n)
+void Sieve(int n)
 {
     for (int i = 3; i <= n; i += 2)
     {
@@ -41,57 +94,46 @@ void sieve(int n)
     }
 }
 
-vector<vector<ll>> PrimePower(1123);
-vector<ll> NOD(1123);
-vector<pair<ll, ll>> v;
-
-void Boom(int t)
+ll SOD(ll n)
 {
-    ll n;
-    cin >> n;
-    cout << "Case " << t << ": " << -1 * v[n - 1].second << endl;
+    ll ans = 1;
+    for (auto p : primes)
+    {
+        if (1LL * p * p > n)
+        {
+            break;
+        }
+        else if (n % p == 0)
+        {
+            ll CurrentSum = 1, PowerOfPrime = 1;
+            while (n % p == 0)
+            {
+                PowerOfPrime *= p;
+                CurrentSum += PowerOfPrime;
+                n /= p;
+            }
+            ans *= CurrentSum;
+        }
+    }
+    if (n > 1)
+    {
+        ans *= (n + 1);
+    }
+    return ans;
 }
 
 int main()
 {
     Boost;
-    int lim = 1e3;
-    sieve(lim);
-
-    for (auto p : primes)
-    {
-        for (int i = p; i <= lim; i += p)
-        {
-            int cnt = 1, tmp = i;
-            while (tmp % p == 0)
-            {
-                tmp /= p;
-                cnt++;
-            }
-            PrimePower[i].push_back(cnt);
-        }
-    }
-
-    for (int i = 1; i <= lim; i++)
-    {
-        NOD[i] = 1;
-        for (auto u : PrimePower[i])
-        {
-            NOD[i] *= u;
-        }
-    }
-    for (int i = 1; i <= lim; i++)
-    {
-        v.push_back({NOD[i], -i});
-    }
-
-    sort(v.begin(), v.end());
-
+    Sieve(1e8);
     int t;
     cin >> t;
-    for (int i = 1; i <= t; i++)
+    while (t--)
     {
-        Boom(i);
+        ll n;
+        cin >> n;
+        cout << SOD(n) - n << endl;
     }
+
     return 0;
 }
