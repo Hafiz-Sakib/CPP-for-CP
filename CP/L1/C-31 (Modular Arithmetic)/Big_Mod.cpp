472a28fb54e553af746abfddb5298ca1a64e1410
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-06-02 20:26:24 +0600

    BigMod UVA Solved

--- CP/L1/C-31 (Modular Arithmetic)/Big_Mod.cpp
+++ CP/L1/C-31 (Modular Arithmetic)/Big_Mod.cpp
@@ -1,13 +1,14 @@
+// https://vjudge.net/problem/UVA-374
+
 #include <bits/stdc++.h>
 using namespace std;
-#define mod 1000000007
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-int BinaryExponentiation(int b, int p)
+int BinaryExponentiation(int b, int p, int mod)
 {
     int ans = 1;
     while (p)
@@ -25,8 +26,10 @@ int BinaryExponentiation(int b, int p)
 int main()
 {
     Boost;
-    int base, power;
-    cin >> base >> power;
-    cout << BinaryExponentiation(base, power) << endl;
+    int b, p, m;
+    while (cin >> b >> p >> m)
+    {
+        cout << BinaryExponentiation(b, p, m) << endl;
+    }
     return 0;
 }
