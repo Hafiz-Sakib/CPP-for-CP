21589f3c7f41d22918db37b3cc546e2b84eac302
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-06-25 22:56:02 +0600

    Level 01 Completed

---
+++ CP/L2/Number Theory/11_Euler Phi Extension Theorem/Problem Solving/test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define mod 1000000007
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+}
+
+int main()
+{
+    Boost;
+    Boom();
+    return 0;
+}
