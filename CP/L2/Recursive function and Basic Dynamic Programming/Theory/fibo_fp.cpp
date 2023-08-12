0438c76c0a79a50384b960cb4bef285016e57293
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-08-12 15:38:54 +0600

    DP

---
+++ CP/L2/Recursive function and Basic Dynamic Programming/Theory/fibo_fp.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[1000];
+int fibo(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    else if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+    else
+    {
+        return dp[n] = fibo(n - 1) + fibo(n - 2);
+    }
+}
+int main()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        dp[i] = -1;
+    }
+    int n;
+    cin >> n;
+    for (int i = 0; i <= n; i++)
+    {
+        cout << fibo(i) << ' ';
+    }
+
+    return 0;
+}
