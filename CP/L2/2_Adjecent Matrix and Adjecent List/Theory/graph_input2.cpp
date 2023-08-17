d494f170db9469fdac5ed757ecb7f2c91919ff84
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-08-17 23:18:19 +0600

    Graph Input

--- CP/L2/2_Adjecent Matrix and Adjecent List/Theory/graph_input2.cpp
+++ CP/L2/2_Adjecent Matrix and Adjecent List/Theory/graph_input2.cpp
@@ -4,7 +4,7 @@
 সরাসরি ম্যাট্রিক্স না দিয়ে নোড সংখ্যা,edge সংখ্যা বলে দিয়ে edge গুলো কি কি বলে দিতে পারে,এভাবে:
 
 3 3  //৩ টা নোড এবং ৩টা এজ
-1 2 5 //node1-node2-cost
+1 2 5 //node1-node2-cost => 1 to 2 -> Cost 5 (Vice-Versa because Bi-Directional)
 2 3 8
 1 3 3
 
@@ -18,14 +18,13 @@ int main()
 {
     int Node, Edge;
     int matrix[100][100];
-    scanf("%d%d", &Node, &Edge);
+    cin >> Node >> Edge;
     for (int i = 0; i < Edge; i++)
     {
         int n1, n2, cost;
-        scanf("%d%d%d", &n1, &n2, &cost);
-        matrix[n1][n2] = cost;
-        matrix[n2][n1] = cost;
-
+        cin >> n1 >> n2 >> cost;
+        matrix[n1][n2] = cost; // n1 to n2 cost = cost
+        matrix[n2][n1] = cost; // n2 to n1 cost = cost
         return 0;
     }
 }
