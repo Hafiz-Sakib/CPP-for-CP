92663b4df0e9593319487d92daae38143e2113ee
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-10-31 23:01:36 +0600

    Graph Representation

--- CP/L2/2_Adjecent Matrix and Adjecent List/Theory/adc_list.cpp
+++ CP/L2/2_Adjecent Matrix and Adjecent List/Theory/adc_list.cpp
@@ -3,9 +3,14 @@ using namespace std;
 const int N = 1e3 + 10;
 int main()
 {
-    vector<vector<int>> v(N);
+    vector<vector<int>> graph(N);
     int n, e;
     cin >> n >> e;
+    for (int i = 1; i <= e; i++)
+    {
+        int v1, v2;
+        graph[v1].push_back(v2);
+    }
 
     return 0;
 }
