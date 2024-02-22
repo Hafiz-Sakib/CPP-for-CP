4430f874465ce41a4cdb1ac2de85aba89c40fa39
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-02-22 18:25:48 +0600

    test

--- CP/L2/3_DFS/Theory/test.cpp
+++ CP/L2/3_DFS/Theory/test.cpp
@@ -19,119 +19,87 @@ typedef vector<vector<ll>> vvi;
 typedef vector<pair<ll, ll>> vpii;
 typedef vector<pair<string, ll>> vpsi;
 typedef pair<ll, ll> pii;
+typedef pair<string, ll> psi;
+typedef pair<ll, string> pis;
+typedef pair<double, double> pdd;
 typedef map<ll, ll> mii;
+typedef map<ll, bool> mib;
 typedef map<string, ll> msi;
+typedef map<ll, string> mis;
 typedef set<ll> si;
 typedef set<char> sc;
 typedef set<string> ss;
+typedef set<double> sd;
+typedef priority_queue<ll> pq;
+typedef priority_queue<string> pqs;
+typedef priority_queue<ll, vi, greater<ll>> pqg;
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
 
 #define pb push_back
 #define mp make_pair
+#define pob pop_back
 #define in insert
 #define fi first
 #define se second
 #define space ' '
 #define endl "\n"
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
+#define lb lower_bound
+#define ub upper_bound
+
+#define left(node) (node * 2)
+#define mid(l, r) ((r + l) >> 1)
+#define right(node) (node * 2 + 1)
 
 #define b() begin()
 #define e() end()
+#define sz(x) (int)x.size()
 #define all(data) data.begin(), data.end()
 #define rall(data) data.rbegin(), data.rend()
+#define Reverse(data) reverse(data.begin(), data.end())
 #define vecMax(data) *max_element(data.begin(), data.end())
 #define vecMin(data) *min_element(data.begin(), data.end())
 #define vecSum(data) accumulate(data.begin(), data.end(), 0)
+#define vecCount(data, key) count(data.begin(), data.end(), key)
 
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define YES cout << "Yes\n"
+#define NO cout << "No\n"
+
+#define setp(n) fixed << setprecision(n)
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define mem(arr, val) memset(arr, val, sizeof(arr));
 #define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define FOR(data) for (auto it = data.begin(); it != data.end(); it++)
 #define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
 #define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
 
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a * b) / gcd(a, b))
-#define mod 1000000007
-const int mx = 1e8 + 123;
-const double eps = 1e-12;
-
 #define debug(x) cerr << x << endl;
 #define here fprintf(stderr, "====I am Here====\n");
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+#define sqr(a) ((a) * (a))
+#define mod 1000000007
+#define mx_int_prime 999999937
+
+const double PI = acos(-1);
+const int mx = 1e8 + 123;
+const double eps = 1e-12;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
 
 #define Boost                         \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
 
-vector<int> parent(mx);
-vector<int> set_size(mx);
-
-void make(int v)
-{
-    parent[v] = v;
-    set_size[v] = 1;
-}
-
-int find(int v)
-{
-    if (v == parent[v])
-    {
-        return v;
-    }
-    /*
-   path compression,
-   setting all parent nodes root node while calling recursively!
-   */
-    return parent[v] = find(parent[v]);
-}
-
-void union_sets(int a, int b)
-{
-    a = find(a);
-    b = find(b);
-    if (a != b)
-    {
-        // Union by set_size
-        if (set_size[a] < set_size[b])
-        {
-            swap(a, b);
-        }
-        parent[b] = a;
-        set_size[a] += set_size[b];
-    }
-}
 void Boom()
 {
-    // Call Disjoint Set Union Functions First
-    int n, e, total_cost = 0;
-    cin >> n >> e;
-    vector<pair<int, pair<int, int>>> edges;
-    for (int i = 0; i < e; i++)
-    {
-        int u, v, wt;
-        cin >> u >> v >> wt;
-        edges.push_back({wt, {u, v}});
-    }
-
-    sort(edges.begin(), edges.end());
-
-    for (int i = 1; i <= n; i++)
-    {
-        make(i);
-    }
-
-    for (auto edge : edges)
-    {
-        int wt = edge.first;
-        int u = edge.second.first;
-        int v = edge.second.second;
-        if (find(u) == find(v))
-        {
-            continue;
-        }
-        union_sets(u, v);
-        total_cost += wt;
-    }
-
-    cout << "Minimum Cost Spanning Tree Cost = " << total_cost << endl;
+    // Let's Move
 }
 
 int main()
@@ -142,6 +110,7 @@ int main()
     cin >> t;
     while (t--)
     {
+        // cout<<"Case "<<t<<" : ";
         Boom();
     }
 
