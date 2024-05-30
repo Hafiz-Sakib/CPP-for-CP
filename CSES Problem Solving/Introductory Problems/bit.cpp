0ab759844f8147f71ff59630b943f3b72344e14a
Hafiz_Sakib <hafizsakib5@gmail.com>
2024-05-30 21:49:03 +0600

    bits

---
+++ CSES Problem Solving/Introductory Problems/bit.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /*
+        calculate 2^n
+
+        int a = (1 << n);
+    */
+
+    /*
+       check if a number n is power of 2 (x = 2^n ?)
+
+         int b = (n && (!(n & (n - 1))));
+    */
+
+    /*
+       swap two numbers ( a , b )
+           a = a ^ b;
+           b = a ^ b;
+           a = a ^ b;
+    */
+
+    /*
+    Multiply By Power of Two :
+            int x = a << 1; // It means (a * 2^1)
+            int y = a << 4; // It means (a * 2^4)
+            int z = a << 6; // It means (a * 2^6)
+            int n = a << 8; // It means (a * 2^8)
+    */
+
+    /*
+            Divided by Power of two :
+
+            int x = a >> 1; // It means (a/2^1)
+            int y = a >> 4; // It means (a/2^4)
+            int z = a >> 6; // It means (a/2^6)
+            int n = a >> 8; // It means (a/2^8)
+    */
+
+    return 0;
+}
